84c4e8a70c0e94ebb142fa63da724ab7bfb1e59c
SUGHAN <118059255+SUGHAN14@users.noreply.github.com>
2024-03-05 19:48:31 +0530

    Create PatternPrinter.java

---
+++ DC/PatternPrinter.java
@@ -0,0 +1,10 @@
+public class PatternPrinter {
+    public static void main(String[] args) {
+        int n = 5; // Change this value to print a different number of elements
+        for (int i = 1; i <= n; i++) {
+            int result = i * i + 1;
+            System.out.print(result + " ");
+        }
+    }
+}
+// output pattern 2 5 10 17 26 37 50 .....

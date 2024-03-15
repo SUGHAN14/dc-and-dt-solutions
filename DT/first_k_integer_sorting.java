f1021c7ea68c2e8d41731b8c19b6ae1954c2a877
SUGHAN <118059255+SUGHAN14@users.noreply.github.com>
2024-03-15 20:11:49 +0530

    Create first_k_integer_sorting.java

---
+++ DT/first_k_integer_sorting.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        int k = scanner.nextInt();
+        
+        Arrays.sort(arr, 0, Math.min(k, n)); // Sort the first K elements
+        
+        boolean isSorted = true;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1]) {
+                isSorted = false;
+                break;
+            }
+        }
+        
+        if (isSorted) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
